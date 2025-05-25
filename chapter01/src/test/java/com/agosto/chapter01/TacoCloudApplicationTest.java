3c7be6821e2520482ab88b975b8ad0da516ddea9
RodolfoAgosto <rodolfo.agosto@gmail.com>
2025-05-24 23:07:19 -0300

    Terminado capítulo 1 - estructura y dependencias.

---
+++ chapter01/src/test/java/com/agosto/chapter01/TacoCloudApplicationTest.java
@@ -0,0 +1,14 @@
+package com.agosto.chapter01;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
